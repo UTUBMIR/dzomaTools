0be40df55af8b02de615ed3f30addb825572bad6
Illya Dzoma <105543974+UTUBMIR@users.noreply.github.com>
2024-11-03 11:54:47 +0100

    Add files via upload

--- dzomaTools.cpp
+++ dzomaTools.cpp
@@ -1,4 +1,4 @@
-#include "dzomaTools.h"//0.01
+#include "dzomaTools.h"//v0.01
 
 void dzomaTools::inputNumbers(int arr[], unsigned short numbers, const char* text, short changeCounter) {
 	for (char i = 0; i < numbers; ++i) {
@@ -16,16 +16,12 @@ void dzomaTools::inputNumbers(int arr[], unsigned short numbers, const char* tex
 	system("cls");
 }
 
-void dzomaTools::inputNumbers(int arr[], unsigned short numbers, const char* text, short changeCounter, const char* addition[]) {
+void dzomaTools::inputNumbers(double arr[], unsigned short numbers, const char* text, short changeCounter) {
 	for (char i = 0; i < numbers; ++i) {
 		while (true) {
 			system("cls");
-			char* buffer = new char[strlen(text) + strlen(addition[i]) + 3];
-			sprintf_s(buffer, strlen(text) + strlen(addition[i]) + 3, "%s%s", text, addition[i]);
-			printf(buffer, i + changeCounter);
-
-			delete[] buffer;
-			if (scanf_s("%d", &arr[i]) != 1) {
+			printf(text, i + changeCounter);
+			if (scanf_s("%lf", &arr[i]) != 1) {
 				while (getchar() != '\n');
 			}
 			else {
@@ -36,12 +32,17 @@ void dzomaTools::inputNumbers(int arr[], unsigned short numbers, const char* tex
 	system("cls");
 }
 
-void dzomaTools::inputNumbers(double arr[], unsigned short numbers, const char* text, short changeCounter) {
+
+void dzomaTools::inputNumbers(int arr[], unsigned short numbers, const char* text, short changeCounter, const char* addition[]) {
 	for (char i = 0; i < numbers; ++i) {
 		while (true) {
 			system("cls");
-			printf(text, i + changeCounter);
-			if (scanf_s("%lf", &arr[i]) != 1) {
+			char* buffer = new char[strlen(text) + strlen(addition[i]) + 3];
+			sprintf_s(buffer, strlen(text) + strlen(addition[i]) + 3, "%s%s", text, addition[i]);
+			printf(buffer, i + changeCounter);
+
+			delete[] buffer;
+			if (scanf_s("%d", &arr[i]) != 1) {
 				while (getchar() != '\n');
 			}
 			else {
@@ -70,4 +71,42 @@ void dzomaTools::inputNumbers(double arr[], unsigned short numbers, const char* 
 		}
 	}
 	system("cls");
+}
+
+
+
+void dzomaTools::minInDiapason(int arr[], size_t start, size_t end) {
+	int min = arr[start];
+	for (++start; start < end; ++start) {
+		if (arr[start] < min) {
+			min = arr[start];
+		}
+	}
+}
+
+void dzomaTools::maxInDiapason(int arr[], size_t start, size_t end) {
+	int max = arr[start];
+	for (++start; start < end; ++start) {
+		if (arr[start] > max) {
+			max = arr[start];
+		}
+	}
+}
+
+void dzomaTools::minInDiapason(double arr[], size_t start, size_t end) {
+	int min = arr[start];
+	for (++start; start < end; ++start) {
+		if (arr[start] < min) {
+			min = arr[start];
+		}
+	}
+}
+
+void dzomaTools::maxInDiapason(double arr[], size_t start, size_t end) {
+	int max = arr[start];
+	for (++start; start < end; ++start) {
+		if (arr[start] > max) {
+			max = arr[start];
+		}
+	}
 }
--- dzomaTools.h
+++ dzomaTools.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <iostream>
-namespace dzomaTools {//0.01
+namespace dzomaTools {//v0.01
 	void inputNumbers(int arr[], unsigned short numbers, const char* text, short changeCounter);
-	void inputNumbers(int arr[], unsigned short numbers, const char* text, short changeCounter, const char* addition[]);
-
 	void inputNumbers(double arr[], unsigned short numbers, const char* text, short changeCounter);
+
+	void inputNumbers(int arr[], unsigned short numbers, const char* text, short changeCounter, const char* addition[]);
 	void inputNumbers(double arr[], unsigned short numbers, const char* text, short changeCounter, const char* addition[]);
+
+
+	void minInDiapason(int arr[], size_t start, size_t end);
+	void maxInDiapason(int arr[], size_t start, size_t end);
+
+	void minInDiapason(double arr[], size_t start, size_t end);
+	void maxInDiapason(double arr[], size_t start, size_t end);
 };
 
